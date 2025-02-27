7975e1ece250c1dc7477c5f011e9e086a6476a6d
Raymond Chong <mathsgod@yahoo.com>
2025-02-27 10:31:11 +0800

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'acice.hlhk.net',
+  appName: 'ACICE',
+  webDir: 'www'
+};
+
+export default config;
